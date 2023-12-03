959dc4418cd2cea7e07d852f0ca74cab85ac792a
kali-mx <76034874+kali-mx@users.noreply.github.com>
2023-12-03 01:27:18 -0800

    Update hcat_speed_run.py
    - now menu driven
    - built in benchmark option
    - compatible with multi-GPUs
    - banner

--- hcat_speed_run.py
+++ hcat_speed_run.py
@@ -1,120 +1,216 @@
 #!/usr/bin/python3
 
-#author: Max Ahartz
-#created: 12/01/2023
+# author: Max Ahartz
+# created: 12/01/2023
 
-#create the data.txt file by first running hashcat -b on the system you want to benchmark.  
-#Then copy and paste the screen output to the new file.
+# create the data.txt file by first running hashcat -b on the system you want to benchmark.
+# Then copy and paste the screen output to the new file.
+# OR follow th user prompts to benchmark your system!
 
-password_length = 7 #CHANGE THIS!
-total_combos = 62   #CHANGE THIS!
+import os, sys, pyfiglet, subprocess, time, threading
 
-myfile = 'data.txt'
+
+class ProgressBarLoading(threading.Thread):
+    def run(self):
+        global stop
+        global kill
+        print('Loading....  ', end='')
+        sys.stdout.flush()
+        i = 0
+        while not stop:
+            if (i % 4) == 0:
+                sys.stdout.write('\b/')
+            elif (i % 4) == 1:
+                sys.stdout.write('\b-')
+            elif (i % 4) == 2:
+                sys.stdout.write('\b\\')
+            elif (i % 4) == 3:
+                sys.stdout.write('\b|')
+
+            sys.stdout.flush()
+            time.sleep(0.2)
+            i += 1
+
+        if kill:
+            print('\b\b\b\b ABORT!')
+        else:
+            print('\b\b done!')
+
+
+def run_with_progress_bar():
+    global stop, kill
+    kill = False
+    stop = False
+    p = ProgressBarLoading()
+    p.start()
+
+    try:
+        # Anything you want to run.
+        start_time = time.time()
+        print("Benchmarking your system now with hashcat -b. This will take about 8 minutes...") 
+        os.system("hashcat -b >data2.txt")
+        print("elapsed time:", time.time() - start_time)
+        time.sleep(1)
+        stop = True
+    except (KeyboardInterrupt, EOFError):
+        kill = True
+        stop = True
+
+
+myfile = 'data2.txt'
+
 table_data = []
+answers = ["Y", 'y', "yes", 'Yes']
+password_length = int(input("Enter password length: "))
+total_combos = int(input("Enter permutations: "))
+answer = input("Benchmark now? Y/n: ")
+if answer in answers:
+    run_with_progress_bar()
+else:
+    myfile = input("Enter your existing benchmark file: ")
 
-import os, sys, pyfiglet 
-from datetime import datetime
 os.system("clear")
 
-banner = pyfiglet.figlet_format("Hcat_speed_run!")
-print(banner)
-print("python version:", sys.version)
-current_date = datetime.now()
-print(current_date)
-date = datetime.today().strftime("%Y-%m-%d")
+
+print("""
+ _               _                               _                      _ 
+| |__   ___ __ _| |_     ___ _ __   ___  ___  __| |    _ __ _   _ _ __ | |
+| '_ \ / __/ _` | __|   / __| '_ \ / _ \/ _ \/ _` |   | '__| | | | '_ \| |
+| | | | (_| (_| | |_    \__ \ |_) |  __/  __/ (_| |   | |  | |_| | | | |_|
+|_| |_|\___\__,_|\__|___|___/ .__/ \___|\___|\__,_|___|_|   \__,_|_| |_(_)
+                   |_____|  |_|                  |_____|                  
+
+""")
 
 def human_readable(total_variations):
     if len(str(total_variations)) == 13:
         number = str(total_variations)
-        formatted_number = number[0] + '.' + number[1]
+        formatted_number = number[0] + "." + number[1]
         return formatted_number + " trillion."
 
     elif len(str(total_variations)) == 14:
         number = str(total_variations)
-        formatted_number = number[0:2] + '.' + number[2]
-        return formatted_number + " trillion." 
+        formatted_number = number[0:2] + "." + number[2]
+        return formatted_number + " trillion."
 
     elif len(str(total_variations)) == 15:
         number = str(total_variations)
-        formatted_number = number[0:3] + '.' + number[3]
+        formatted_number = number[0:3] + "." + number[3]
         return formatted_number + " trillion."
     else:
         return "Bud, it's pointless. You won't be alive to see it with today's current compute power."
 
 
-total_variations = int(total_combos ** password_length)
-print('\n')
-print(f"{password_length} character password with {total_combos} permutations:")
-print("Total possibilities to run is:",total_variations, 'or about', str(human_readable(total_variations)),'\n')
+def math(hash_rate, hash_int, total_variations):
+    if "GH/s" in hash_rate:
+        GH_to_H = hash_int * 10**9
+        time_to_crack = total_variations / GH_to_H
 
-with open(myfile, 'r') as file:
+    elif "MH/s" in hash_rate:
+        MH_to_H = hash_int * 10**6
+        time_to_crack = total_variations / MH_to_H
+
+    elif "kH/s" in hash_rate:
+        kH_to_H = hash_int * 10**3
+        time_to_crack = total_variations / kH_to_H
+
+    else:
+        Hash_per_sec = hash_int
+        time_to_crack = total_variations / Hash_per_sec
+
+    time_to_crack = round(time_to_crack, 1)
+    hr_to_crack = round(time_to_crack / 3600, 1)
+    days_to_crack = round(hr_to_crack / 24, 1)
+
+    # Store data in a dictionary
+    row_data = {
+        "Mode": mode_number,
+        "Mode Name": mode_name,
+        "Hash Rate": hash_rate,
+        "Time(sec)": time_to_crack,
+        "Hours": hr_to_crack,
+        "Days": days_to_crack,
+    }
+
+    # Append the dictionary to the list
+    table_data.append(row_data)
+
+    return table_data
+
+
+total_variations = int(total_combos**password_length)
+print("\n")
+print(f"{password_length} character password with {total_combos} permutations:")
+print(
+    "Total possibilities to run is:",
+    total_variations,
+    "or about",
+    str(human_readable(total_variations)),
+    "\n",
+)
+
+
+with open(myfile, "r") as file:
     for _ in range(20):
         line = file.readline()
-        print(line.strip()) 
+        print(line.strip())
 
 # Open the file and iterate through lines
-with open(myfile, 'r') as file:
-  
-  # Print table heading
-    print("{:<10} {:<45} {:<25} {:<20} {:<20} {:<10}".format("Mode", "Mode Name", "Hash Rate", "Time(sec)", "Hours", "Days"))
+with open(myfile, "r") as file:
+
+    # Print table heading
+    print(
+        "{:<10} {:<45} {:<25} {:<20} {:<20} {:<10}".format(
+            "Mode", "Mode Name", "Hash Rate", "Time(sec)", "Hours", "Days"
+        )
+    )
     print("-" * 129)
 
     for line in file:
         # Exclude text from '[Iterations' until the end of the line
-        line = line.split('[Iterations')[0].strip()
+        line = line.split("[Iterations")[0].strip()
+
         # Check for lines containing "Hash-Mode" and extract information
         if "Hash-Mode" in line:
             parts = line.split()
             mode_number = parts[2]
             mode_name = " ".join(parts[3:])
-        elif "Speed.#1" in line:
+
+        if "Speed.#1" in line or "Speed.#*" in line:
             parts = line.split()
             hash_rate = " ".join(parts[1:3])
-            hash_int = float(parts[1])
-            
+            hash_int = int(float(parts[1]))
+            math(hash_rate, hash_int, total_variations)
 
-            if "GH/s" in hash_rate:
-                GH_to_H = hash_int * 10**9
-                time_to_crack = total_variations / GH_to_H 
 
-            elif "MH/s" in hash_rate:
-                MH_to_H = hash_int * 10**6
-                time_to_crack = total_variations / MH_to_H
+unique_modes = {}  # Dictionary to store unique 'Mode' entries with minimum 'Time(sec)'
 
-            elif "kH/s" in hash_rate:
-                kH_to_H = hash_int * 10**3
-                time_to_crack = total_variations / kH_to_H  
+for entry in table_data:
+    mode = entry["Mode"]
+    time_sec = entry["Time(sec)"]
 
-            else:
-                Hash_per_sec = hash_int
-                time_to_crack = total_variations / Hash_per_sec
+    # If the 'Mode' is not in the dictionary or the current 'Time(sec)' is smaller, update the entry
+    if mode not in unique_modes or time_sec < unique_modes[mode]["Time(sec)"]:
+        unique_modes[mode] = entry
 
-            time_to_crack = round(time_to_crack)
-            hr_to_crack = round(time_to_crack/3600)
-            days_to_crack = round(hr_to_crack/24)
-           
-            #print("{:<10} {:<45} {:<25} {:<20} {:<20} {:<20}".format(mode_number, mode_name, hash_rate, time_to_crack, hr_to_crack, days_to_crack))
-            
-             # Store data in a dictionary
-            row_data = {
-                "Mode": mode_number,
-                "Mode Name": mode_name,
-                "Hash Rate": hash_rate,
-                "Time(sec)": time_to_crack,
-                "Hours": hr_to_crack,
-                "Days": days_to_crack
-            }
 
-            # Append the dictionary to the list
-            table_data.append(row_data)
+# Convert the dictionary values back to a list
+table_data = list(unique_modes.values())
 
-    # Sort the table data by the "Time(sec)" key (fastest times first)
-    sorted_table_data = sorted(table_data, key=lambda x: x["Time(sec)"])
+# Sort the table data by the "Time(sec)" key (fastest times first)
+# list
+sorted_table_data = sorted(table_data, key=lambda x: x["Time(sec)"])
 
-    # Print the sorted table
-    for row in sorted_table_data:
-        print("{:<10} {:<45} {:<25} {:<20} {:<20} {:<10}".format(
-            row["Mode"], row["Mode Name"], row["Hash Rate"],
-            row["Time(sec)"], row["Hours"], row["Days"]
-        ))
-   
+
+# Print the sorted table
+for row in sorted_table_data:
+    print(
+        "{:<10} {:<45} {:<25} {:<20} {:<20} {:<10}".format(
+            row["Mode"],
+            row["Mode Name"],
+            row["Hash Rate"],
+            row["Time(sec)"],
+            row["Hours"],
+            row["Days"],
+        )
+    )
